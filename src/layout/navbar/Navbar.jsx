2818faa1a19da1383ecfa0d72e775bf857b0508c
dressgecko <dressgecko@gmail.com>
2024-03-27 02:01:15 +0500

    added buttons to navbar

--- src/layout/navbar/Navbar.jsx
+++ src/layout/navbar/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Bars3Icon } from '@heroicons/react/24/outline'
+import ButtonBars3 from "./button_bars_3/ButtonBars3"
+import ButtonProfile from "./button_profile/ButtonProfile"
+import ButtonAdd from "./button_add/ButtonAdd"
 
 const Navbar = () => {
   return (
     <header className="border-b md:flex md:items-center md:justify-between p-4 pb-0 shadow-lg md:pb-4">
-      
       {/* Button for sidebar and Logo text */}
       <nav>
         <ul className="list-reset md:flex md:items-center">
-          <li className="md:ml-4 flex items-center"> {/* Обертываем содержимое li в flex контейнер */}
-            <button>
-              <Bars3Icon className="h-7 w-7 text-black" />
-            </button>
-            <h1 className="leading-none text-2xl text-grey-darkest ml-2"> {/* Добавляем margin слева для выравнивания */}
-              <a className="no-underline text-grey-darkest hover:text-black" href="#">
+          <li> 
+            <ButtonBars3 />
+          </li>
+          <li>
+            <h1 className="leading-none text-2xl text-grey-darkest ml-5"> {/* Добавляем margin слева для выравнивания */}
+              <a href="#">
                 AdSenseVision
               </a>
             </h1>
@@ -34,19 +35,10 @@ const Navbar = () => {
       <nav>
         <ul className="list-reset md:flex md:items-center">
           <li className="md:ml-4">
-            <a className="block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
-              Products
-            </a>
+          <ButtonAdd />
           </li>
           <li className="md:ml-4">
-            <a className="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
-              About
-            </a>
-          </li>
-          <li className="md:ml-4">
-            <a className="border-t block no-underline hover:underline py-2 text-grey-darkest hover:text-black md:border-none md:p-0" href="#">
-              Contact
-            </a>
+          <ButtonProfile />
           </li>
         </ul>
       </nav>
