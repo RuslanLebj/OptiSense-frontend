14bc15b52614572658c7a6b950b516a22f1cb91c
dressgecko <dressgecko@gmail.com>
2024-05-08 02:42:40 +0500

    classname fix

--- src/components/cards/statisticsCard/StatisticsCard.jsx
+++ src/components/cards/statisticsCard/StatisticsCard.jsx
@@ -1,14 +1,14 @@
 const StatisticsCard = ({ stat }) => {
     return (
-        <div class="max-w-lg w-full rounded-lg shadow-lg p-4 my-4">
-            <h3 class="font-semibold text-lg tracking-wide">{stat.screen_detail.name}</h3>
-            <p class="text-gray-500 my-1">
+        <div className="max-w-lg w-full rounded-lg shadow-lg p-4 my-4">
+            <h3 className="font-semibold text-lg tracking-wide">{stat.screen_detail.name}</h3>
+            <p className="text-gray-500 my-1">
                 Число показов: {stat.show_count}
             </p>
-            <p class="text-gray-500 my-1">
+            <p className="text-gray-500 my-1">
                 Общее время просмотра: {stat.total_viewing_time}
             </p>
-            <p class="text-gray-500 my-1">
+            <p className="text-gray-500 my-1">
                 Максимальное количество зрителей: {stat.max_viewers_count}
             </p>
         </div>
